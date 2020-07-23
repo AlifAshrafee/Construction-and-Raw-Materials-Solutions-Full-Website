86d2df4d0755d0edc378664e8ddd23e329f7a847
Reeshoon Sayera <reeshoon@gmail.com>
2020-07-23 23:30:21 +0600

    catalogue application updated

--- AppLanding/catalogue/views.py
+++ AppLanding/catalogue/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+# Create your views here.
+def architecture(request):
+    return render(request,'catalogue/ArchiCatalogue.html')
